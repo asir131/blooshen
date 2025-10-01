a040dac8667ec0e0b48d7feab1e03af86d8a0953
asir131 <afaysal220@gmail.com>
2025-10-02 01:24:27 +0600

    dependencies

--- src/app/dashboard/add-inventory/continue-add-inventory/page.tsx
+++ src/app/dashboard/add-inventory/continue-add-inventory/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md"
-import { Button } from "../../../../components/ui/button"
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
